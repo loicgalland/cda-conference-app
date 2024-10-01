8cca3a825587da2710a447547d0eda2cf388cc30
loicgalland <loicgalland1@gmail.com>
2024-10-01 14:16:07 +0200

    Basic authenticator test

---
+++ src/adapters/in-memory-user-repository.ts
@@ -0,0 +1,15 @@
+import {IUserRepository} from "../ports/user-repository.interface";
+import {User} from "../entities/user.entity";
+
+export class InMemoryUsersRepository implements IUserRepository {
+    private users: User[] = []
+
+    async create(user: User): Promise<void> {
+        this.users.push(user)
+    }
+
+    async findByEmailAddress(emailAddress: string): Promise<User | null> {
+        const user = this.users.find(user => user.props.emailAddress === emailAddress);
+        return user ?? null
+    }
+}
